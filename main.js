fd9d0f7b215b493f956a685e054cea3c63a72790
Jacob Vilevac <jakevilevac@gmail.com>
2023-03-30 04:42:37 -0700

    adjusted scroll and colors

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const container = document.querySelector('.left-section-bg');
 
 window.addEventListener('scroll', () => {
-    const scrollWidth = window.scrollY / 15; // adjust the division factor as needed
-    const maxScrollWidth = Math.min(scrollWidth, 60);
+    const scrollWidth = -30 + (window.scrollY / 10); // adjust the division factor as needed
+    const maxScrollWidth = Math.max(0, Math.min(scrollWidth, 60));
     let viewWidth = 40;
     container.style.width = `${viewWidth + maxScrollWidth}vw`;
 });
